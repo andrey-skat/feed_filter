369ee157a7dfc441993fe38d12f68ef58fe5842a
Andrey Skuryatin <andrey2004@gmail.com>
2013-05-22 16:07:00 +1100

    added example rules

--- rules.rb
+++ rules.rb
@@ -3,14 +3,10 @@ FeedFilter.configure do
 
   #show only AVC and bigger than 2GB
   feed 'http://feed.rutracker.org/atom/f/2093.atom' do |item|
-
-    m = /\[([0-9]*\.[0-9]*) gb\]/i.match(item.title.to_s)
-    if !m.nil? && !m.captures[0].nil?
-      m.captures[0].to_f >= 2 && (item.title.to_s =~ /avc/i || item.title.to_s =~ /h\.264/i)
-    else
-      false
-    end
-
+    filter_movies(item)
+  end
+  feed 'http://feed.rutracker.org/atom/f/2200.atom' do |item|
+    filter_movies(item)
   end
 
   #filter out only those articles that mention Ruby
@@ -20,4 +16,13 @@ FeedFilter.configure do
     item.title.to_s =~ /ruby/i
   end
 
+end
+
+def filter_movies(item)
+  m = /\[([0-9]*\.[0-9]*) gb\]/i.match(item.title.to_s)
+  if !m.nil? && !m.captures[0].nil?
+    m.captures[0].to_f >= 2 && (item.title.to_s =~ /avc/i || item.title.to_s =~ /h\.264/i)
+  else
+    false
+  end
 end
